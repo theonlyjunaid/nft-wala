cb3a5c3ee74a751132face5f35307dea0c32146d
Junaid <onlyjunaid2002@gmail.com>
2024-01-19 21:23:59 +0530

    env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,15 @@
-// const fs = require('fs');
+const fs = require('fs');
 
 require('@nomiclabs/hardhat-ethers');
 
-// const privateKey = fs.readFileSync('.secret').toString().trim();
+const privateKey = fs.readFileSync('.secret').toString().trim();
 
 module.exports = {
   defaultNetwork: 'sepolia',
   networks: {
     sepolia: {
       url: 'https://eth-sepolia.g.alchemy.com/v2/phODLHFjD-NE7Sbr5FVltpGn90zE3DI8',
-      accounts: ['99e5ddcb22c3e2ddd99edf637961329b2331bfa90a7a2517d7b38f2c16166b7a'],
+      accounts: [privateKey],
     },
     hardhat: {
       chainId: 1337,
