4e3d5b1b2685f1ef6b03e58450b8acd14df322b0
Junaid <onlyjunaid2002@gmail.com>
2024-01-21 12:42:50 +0530

    new contract deployed

--- context/constants.js
+++ context/constants.js
@@ -1,5 +1,5 @@
 import market from './NFTMarketplace.json';
 
-export const MarketAddress = '0x931E463B464E68Cf0aF82b956155aa5CFc3D92dC';
+export const MarketAddress = '0x0b317A16cd7D527Ee76C735fD71506E6b39e0d94';
 export const MarketAddressABI = market.abi;
 // 0x5FbDB2315678afecb367f032d93F642f64180aa3
