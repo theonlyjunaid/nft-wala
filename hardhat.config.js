93acdc0edc60c192bddeb710c16c348d80c3f519
Junaid <onlyjunaid2002@gmail.com>
2024-05-12 16:09:13 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,15 +13,7 @@ module.exports = {
     },
     hardhat: {
       chainId: 1337,
-    },
-    // mumbai: {
-    //   url: 'https://rpc-mumbai.maticvigil.com',
-    //   accounts: [privateKey],
-    // },
-    // rinkeby: {
-    //   url: 'https://rinkeby.infura.io/v3/bed4fdcc76bb4978a9a3103ef0946f64',
-    //   accounts: [privateKey],
-    // },
+    }
   },
   solidity: '0.8.4',
 };
